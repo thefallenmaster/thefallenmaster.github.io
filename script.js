2a6c26e4938a9ccd5def977677fdac786ea2f930
Gauthambala <gauthambala2006.2@gmail.com>
2025-12-25 08:45:00 +0530

    Refactor modal functionality in JavaScript and defer script loading in HTML for improved performance

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-<script>
+window.showService = function () {
+    openModal("Currently not available");
+};
 
-    function showService() {
-        openModal("Currently not available");
-    }
+window.showSkills = function () {
+    openModal("Currently Learning");
+};
 
-    function showSkills() {
-        openModal("Currently Learning");
-    }
+function openModal(message) {
+    const modal = document.getElementById("modal");
+    const text = document.getElementById("modal-text");
 
-    function openModal(message) {
-        document.getElementById("modal-text").innerText = message;
-        document.getElementById("modal").style.display = "flex";
-    }
+    if (!modal || !text) return;
 
-    function closeModal() {
-        document.getElementById("modal").style.display = "none";
-    }
-</script>
+    text.innerText = message;
+    modal.style.display = "flex";
+}
+
+window.closeModal = function () {
+    const modal = document.getElementById("modal");
+    if (modal) modal.style.display = "none";
+};
